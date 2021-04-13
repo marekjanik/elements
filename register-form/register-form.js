ccbded98ba3cc419a33530d84afe1f0bb74e403a
Marek Janik <marekadamjanik@gmail.com>
2021-04-13 17:26:51 +0200

    added changes to validation functionality

--- register-form/register-form.js
+++ register-form/register-form.js
@@ -1,23 +1,51 @@
 "use strict";
 
+const username = document.form.name;
+const password = document.form.password;
+const confirmation = document.form.confirm;
+const email = document.form.email;
+
 function validate() {
-  if (document.form.name.value === "") {
-    alert("Please write your name!");
-    document.form.name.focus();
+  if (username.value === "") {
+    username.placeholder = "Username is required.";
+    username.classList.add("error");
+    document.addEventListener("click", function () {
+      username.placeholder = "Username";
+      username.classList.remove("error");
+    });
+    username.focus();
     return false;
   }
-  if (document.form.password.value === "") {
-    alert("Please write your password!");
+
+  if (password.value === "") {
+    document.form.password.placeholder = "Password is required.";
+    document.form.password.classList.add("error");
+    document.addEventListener("click", function () {
+      document.form.password.placeholder = "Password";
+      document.form.password.classList.remove("error");
+    });
     document.form.password.focus();
     return false;
   }
-  if (document.form.confirm.value === "") {
-    alert("Please confirm your password!");
+
+  if (confirmation.value === "") {
+    document.form.confirm.placeholder = "Password is required.";
+    document.form.confirm.classList.add("error");
+    document.addEventListener("click", function () {
+      document.form.confirm.placeholder = "Password";
+      document.form.confirm.classList.remove("error");
+    });
     document.form.confirm.focus();
     return false;
   }
-  if (document.form.email.value === "") {
-    alert("Please write your email!");
+
+  if (email.value === "") {
+    document.form.email.placeholder = "Email is required.";
+    document.form.email.classList.add("error");
+    document.addEventListener("click", function () {
+      document.form.email.placeholder = "Password";
+      document.form.email.classList.remove("error");
+    });
     document.form.email.focus();
     return false;
   }
