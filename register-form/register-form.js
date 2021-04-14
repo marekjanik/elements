bfc062e3454f7f229ff26d63d497303ed02dccc2
Marek Janik <marekadamjanik@gmail.com>
2021-04-14 14:36:55 +0200

    added changes to form validation functionality

--- register-form/register-form.js
+++ register-form/register-form.js
@@ -90,35 +90,45 @@ function validate() {
 
 function validateUsername() {
   if (username.value.length > 0 && username.value.length < 6) {
+    username.setCustomValidity("Username must be at least 6 characters long.");
     username.classList.add("error");
   } else {
+    username.setCustomValidity("");
     username.classList.remove("error");
   }
 }
 
 function validatePassword() {
   if (password.value.length > 0 && password.value.length < 6) {
+    password.setCustomValidity("Password must be at least 6 characters long.");
     password.classList.add("error");
   } else {
+    password.setCustomValidity("");
     password.classList.remove("error");
   }
 }
 
 function validateConfirmation() {
   if (confirmation.value !== password.value) {
+    confirmation.setCustomValidity(
+      "Confirm Password and Password must be the same."
+    );
     confirmation.classList.add("error");
   } else {
+    confirmation.setCustomValidity("");
     confirmation.classList.remove("error");
   }
 }
 
 function validateEmail() {
   if (!email.value.match(pattern)) {
+    email.setCustomValidity("Email is invalid.");
     email.classList.add("error");
     document.addEventListener("click", () => {
       email.classList.remove("error");
     });
   } else {
+    email.setCustomValidity("");
     email.classList.remove("error");
   }
 }
